61fbfb65bf5d3a1bc0f54ec19daac95efbeaf4e9
S Roze <sherozem4@gmail.com>
2022-01-03 12:51:47 +0000

    destructuring objects has been deeply analysed and understood

--- script.js
+++ script.js
@@ -11,55 +11,119 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) { 
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+    will be delivered to ${address} at ${time}`);
+  },
 };
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+restaurant.orderDelivery({ 
+  time:'22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
-
-//Switching variables
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
-
-// Receive 2 return values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
-
-// Nested destructuring
-const nested = [2, 4, [5, 6]];
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
-
-// Default values
-const [p=1,q=1,r=1]= [8,9];
-console.log(p,q,r);
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
 
 
 
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
+
+
+
+
+
+
+
+
+// Curly braces are used, as this is what's used to create objects
+const {name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+
+// it is useful to also set default values
+const {menu = [], starterMenu: starters = []} = restaurant
+console.log(menu, starters);
+
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = {a:23, b:7, c:14};
+({a,b} = obj);
+console.log(a,b);
+
+//Nested Objects
+const {fri: {open: o, close: c}} = openingHours;
+console.log(o,c);
+
+
+
+
 
 // const arr = [2,3,4];
 // const a = arr[0];
