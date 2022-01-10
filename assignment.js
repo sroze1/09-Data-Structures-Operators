18a588a13c9bc360c3b4f2b3680c282035956291
S Roze <sherozem4@gmail.com>
2022-01-10 23:24:44 +0000

    refreshing memory on current topics that have been updated

--- assignment.js
+++ assignment.js
@@ -38,47 +38,31 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
-
-  printGoals: function (...playerNames) {
-    console.log(`${playerNames}`);
-    this.score = String(playerNames.length);
-    console.log(this.score);
-  },
 };
 
-// Team 1 and 2, players, goalkeeper, rest of the team
+// 1. Create an array for each team:
+// The reason the two arrays appear separated in the console log is because there are two variables for one players it is being equated to
+// Because players has 2 arrays, it adds one each to the left hand side.
 const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2. For the whole team of team 1, create a variable with the name of the first player - always the keeper -
+//and a second array with the REST of the players
 const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
-
-
-// All players (One array, 22 players)
+// 3. allPlayers array, containing players of both teams
+// Here we have unpacked two arrays (on the right) and repacked them into a new array called allPlayers on the left. This way we have
+// Destructured the two arrays into one and packed it away on the left
 const [...allPlayers] = [...players1, ...players2];
 console.log(allPlayers);
 
-// Adding players to new array, with the REST as the original 11
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// 4. Create a new array, original team plus 3 substitutes, Thiago, Coutinho, Perisic
+// Pack into players1final - the 3 names below, and also the unpacked array - 'players1'
+const [...players1final] = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+console.log(players1final);
 
-// Nested destructuring Making game.odds property
-let { team1, x: draw, team2 } = game.odds;
+// 5. Based on the odds object  create 'team1' 'draw' 'team2';
+// To rename a property without having to use it's eact name in the object, we reference it then use the colon to redefine
+const { team1, x: draw, team2 } = game.odds;
 console.log(team1, draw, team2);
-
-
-//
-function printGoals(playerNames) {
-  console.log(`${[playerNames]}.  `);
-
-  [...playerNames];
-  console.log(` ${[playerNames.length]}   goals were scored.`);
-}
-
-console.log(game.odds.team2 || game.odds.team1);
-
-if(game.odds.team1 < game.odds.team2) { 
-  console.log(game.odds.team1 + 'win ');
-}
-
-
-
-
