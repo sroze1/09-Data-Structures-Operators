316dfbe59a2ccdece285048e50f789d3b15c03e8
S Roze <sherozem4@gmail.com>
2022-01-11 16:07:01 +0000

    assignment completed

--- assignment.js
+++ assignment.js
@@ -52,14 +52,14 @@ const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 
 // 3. allPlayers array, containing players of both teams
-// Here we have unpacked two arrays (on the right) and repacked them into a new array called allPlayers on the left. This way we have
-// Destructured the two arrays into one and packed it away on the left
-const [...allPlayers] = [...players1, ...players2];
+// Here, the allPlayers is equal to an array, which has all of the elements of
+// players1 and players 2 unpacked inside of the array.
+const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
 // 4. Create a new array, original team plus 3 substitutes, Thiago, Coutinho, Perisic
-// Pack into players1final - the 3 names below, and also the unpacked array - 'players1'
-const [...players1final] = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+// players1final = [ the 3 names below, and also the unpacked array - 'players1 ]
+const players1final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
 console.log(players1final);
 
 // 5. Based on the odds object  create 'team1' 'draw' 'team2';
@@ -70,22 +70,20 @@ console.log(team1, draw, team2);
 // 6. Wrire a function printGoals
 // That receives an abritary number of names (NOT an array) and prints them each
 // to the console. along with the number of goals that were scored (number of players passed in)
-function printGoals(...players1) { 
-  console.log( `${players1} were the goal scorers. ${players1.length} goals were scored`);
-}
-
-function printGoals(...players1) { 
-  console.log( `${players1} were the goal scorers. ${players1.length} goals were scored`);
+function printGoals(...players1) {
+  [...game.scored] = [players1];
+  console.log(
+    `${game.scored} were the goal scorers. ${players1.length} goals were scored`
+  );
 }
 
 printGoals(`Yes`, 'My', 'G');
-printGoals('yES', 'cyz');
 
-
-//7. Team with lower odd is more likely to win. 
+//7. Team with lower odd is more likely to win.
 // Print to the console which team is more likely to win, without using if/else or ternary
-if(team2 > team1) { 
-  console.log(team1);
-}
 
-
+// The reason this works is because using the AND operator, if the first statement is true
+// The the code after is executed. This is why it works. See below after reading,
+// It will make sense
+team1 < team2 && console.log('Team 1 is more likely to win');
+team2 < team1 && console.log('Team 2 is more likely to win');
